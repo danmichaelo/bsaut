00dd989840a1456a579286311743fc2ad4e282dd
Dan Michael O. Heggø <danmichaelo@gmail.com>
2021-08-04 19:29:41 +0200

    Add 670: 'Source Data Found'

--- public_html/api.php
+++ public_html/api.php
@@ -424,6 +424,18 @@ class AuthorityRecord
             ];
         }
 
+        // 670: Source Data Found
+        // Example: https://bsaut.toolforge.org/show/13049248
+        // Multiple values: https://bsaut.toolforge.org/show/8036463
+        $data['sources'] = [];
+        foreach ($record->getFields('670') as $field) {
+            $data['sources'][] = [
+                'value' => $field->sf('a'),
+                'via' => $field->sf('b'),
+                'uri' => $field->sf('u'),
+            ];
+        }
+
         // 672: Title Related to the Entity
         $data['titles'] = [];
         foreach ($record->getFields('672') as $field) {
